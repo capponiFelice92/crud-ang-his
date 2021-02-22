c9167262620c1de7b479d71dc30d73035ed1fd9f
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-02-22 18:30:40 +0100

    Inserimento html ed interfaccia

--- crud-ang-his/src/app/app.component.ts
+++ crud-ang-his/src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Automa } from './automa';
+import { Automabile } from './automabile';
+import { AddEvent, AnnullaEvent, ConfermaEvent, ModificaEvent, RicercaEvent, RimuoviEvent, SelezionaEvent } from './state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'crud-ang-his';
+export class AppComponent implements Automabile {
+
+  automa: Automa;
+
+  input1: string;
+  input2: string;
+  inputRicerca: string;
+
+
+  constructor() { this.automa = new Automa(this) };
+
+  nuova() {
+    this.automa.next(new AddEvent);
+  }
+  rimuovi() {
+    this.automa.next(new RimuoviEvent);
+  }
+  modifica() {
+    this.automa.next(new ModificaEvent);
+  }
+  conferma() {
+    this.automa.next(new ConfermaEvent);
+  }
+  annulla() {
+    this.automa.next(new AnnullaEvent);
+  }
+  seleziona() {
+    this.automa.next(new SelezionaEvent);
+  }
+  cerca() {
+    this.automa.next(new RicercaEvent);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
+  entraStatoRicerca() { }
+  entraStatoAggiungi() { }
+  entraStatoVisualizza() { }
+  entraStatoModifica() { }
+  entraStatoRimuovi() { }
+
 }
