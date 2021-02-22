55f44e8df030a0a775ddbd551481bd0d5ed734b6
Matteo Moretti <78532093+matteo2025@users.noreply.github.com>
2021-02-22 21:24:04 +0100

    interazione UI

--- crud-ang-his/src/app/app.component.ts
+++ crud-ang-his/src/app/app.component.ts
@@ -16,37 +16,107 @@ export class AppComponent implements Automabile {
   input2: string;
   inputRicerca: string;
 
+  form: boolean;
+  aggiungi: boolean;
+  remove: boolean;
+  edit: boolean;
+  conf: boolean;
+  annull: boolean;
+  search: boolean;
+  tabella: boolean;
+  codice: boolean;
+  descrizione: boolean;
+
 
   constructor() { this.automa = new Automa(this) };
 
   nuova() {
     this.automa.next(new AddEvent, this.automa);
+    //  this.codice = false;
+    //this.descrizione = false;
   }
   rimuovi() {
-    this.automa.next(new RimuoviEvent);
+    this.automa.next(new RimuoviEvent, this.automa);
   }
   modifica() {
-    this.automa.next(new ModificaEvent);
+    this.automa.next(new ModificaEvent, this.automa);
+
   }
   conferma() {
-    this.automa.next(new ConfermaEvent);
+    this.automa.next(new ConfermaEvent, this.automa);
+
   }
   annulla() {
-    this.automa.next(new AnnullaEvent);
+    this.automa.next(new AnnullaEvent, this.automa);
+
   }
   seleziona() {
-    this.automa.next(new SelezionaEvent);
+    this.automa.next(new SelezionaEvent, this.automa);
   }
   cerca() {
-    this.automa.next(new RicercaEvent);
+    this.automa.next(new RicercaEvent, this.automa);
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
-  entraStatoRicerca() { }
-  entraStatoAggiungi() { }
-  entraStatoVisualizza() { }
-  entraStatoModifica() { }
-  entraStatoRimuovi() { }
+  entraStatoRicerca() {
+    this.form = false;
+    this.aggiungi = true;
+    this.search = true;
+    this.tabella = true;
+    this.codice = false;
+    this.descrizione = false;
+    this.codice = false;
+    this.descrizione = false;
+  }
+  entraStatoAggiungi() {
+    this.form = true;
+    this.aggiungi = false;
+    this.remove = false;
+    this.edit = false;
+    this.conf = true;
+    this.annull = true;
+    this.search = false;
+    this.tabella = false;
+    this.codice = false;
+    this.descrizione = false;
+  }
+  entraStatoVisualizza() {
+    this.form = true;
+    this.aggiungi = true;
+    this.remove = true;
+    this.edit = true;
+    this.conf = false;
+    this.annull = false;
+    this.search = true;
+    this.tabella = true;
+    this.codice = true;
+    this.descrizione = true;
+
+  }
+  entraStatoModifica() {
+    this.form = true;
+    this.aggiungi = false;
+    this.remove = false;
+    this.edit = false;
+    this.conf = true;
+    this.annull = true;
+    this.search = false;
+    this.tabella = false;
+    this.codice = false;
+    this.descrizione = false;
+  }
+  entraStatoRimuovi() {
+    this.form = true;
+    this.aggiungi = false;
+    this.remove = false;
+    this.edit = false;
+    this.conf = true;
+    this.annull = true;
+    this.search = false;
+    this.tabella = false;
+    this.codice = true;
+    this.descrizione = true;
+  }
 
 }
